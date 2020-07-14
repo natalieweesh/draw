a23e338eb432892e8e7451c473f5ef72e7c71740
Natalie Wee <natalieweesh@gmail.com>
2020-07-13 23:27:34 -0400

    first deploy

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState('');
   const [users, setUsers] = useState([]);
 
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://react-simple-chat-app.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
