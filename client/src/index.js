97b1ecf0f15aa8a3d7520ec834f27f13491edd68
Natalie Wee <natalieweesh@gmail.com>
2020-07-19 09:22:00 -0400

    add sentryio for logging

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import * as Sentry from '@sentry/react';
 import App from './App';
+
+Sentry.init({dsn: "https://77d30d679de84c36b37ed3775462f569@o422420.ingest.sentry.io/5348482"});
 
 ReactDOM.render(<App />, document.querySelector('#root'));
